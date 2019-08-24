82dc4338d2b39e656876978cfe85006b02bdf6e7
Georgi Baychev <georgi.baychev@gmail.com>
2019-08-24 12:57:52 +0200

    Version bump

--- NReflect/Properties/AssemblyInfo.cs
+++ NReflect/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NReflect")]
-[assembly: AssemblyCopyright("Copyright © Malte Ried 2010")]
+[assembly: AssemblyCopyright("Copyright © Malte Ried 2010, Georgi Baychev 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.4.0")]
-[assembly: AssemblyFileVersion("0.9.4.0")]
+[assembly: AssemblyVersion("0.9.5.0")]
+[assembly: AssemblyFileVersion("0.9.5.0")]
